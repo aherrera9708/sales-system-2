e4579d34b900a580df93355b773d04c64a5c50fd
Alejandro Herrera Ocampo <alejom21400@hotmail.com>
2021-03-02 19:00:17 -0500

    Screenshots.

---
+++ Sales_2/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    public class Sale
+    {
+        public Product Product;
+        public Seller Seller;
+        public string Comments;
+
+        
+    }
+}
